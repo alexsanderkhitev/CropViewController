b6edc784bfe9edc0e8a5a9dd757f785f77a4c471
Alexsander Khitev <alexsanderskywork@gmail.com>
2017-02-26 15:59:26 +0400

    created framework

---
+++ CropViewController/CropViewController.h
@@ -0,0 +1,19 @@
+//
+//  CropViewController.h
+//  CropViewController
+//
+//  Created by Alexsander Khitev on 2/26/17.
+//  Copyright © 2017 Alexsander Khitev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CropViewController.
+FOUNDATION_EXPORT double CropViewControllerVersionNumber;
+
+//! Project version string for CropViewController.
+FOUNDATION_EXPORT const unsigned char CropViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CropViewController/PublicHeader.h>
+
+
